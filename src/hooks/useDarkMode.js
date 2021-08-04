e15b681c55a989dde20e1b1d5fb44793fbe1617a
sb7297 <sbaireddy7297@gmail.com>
2021-08-04 12:36:13 -0400

    create dark mode hook

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,7 @@
+import useLocalStorage from "./useLocalStorage";
+
+const useDarkMode = () => {
+  const [darkMode, setDarkMode] = useLocalStorage("darkMode", false);
+
+  return [darkMode, setDarkMode];
+}
